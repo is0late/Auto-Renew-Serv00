b8816b7d97b268865b482b9a6379a7954a7c25b9
is0late_cn <34314582+is0late@users.noreply.github.com>
2024-08-06 15:57:56 +0800

    Update script.py

--- script.py
+++ script.py
@@ -1,7 +1,3 @@
-import smtplib
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
-from playwright.sync_api import sync_playwright
 import os
 
 # 从环境变量中获取账户信息和后台序数
